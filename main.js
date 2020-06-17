8c450d7945bdc42ceb4be9c2b9a24fd9c84ff562
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-06-16 23:09:37 -0700

    TS Hello World!

--- main.js
+++ main.js
@@ -8,5 +8,16 @@ exports.__esModule = true;
 var message = 'Welcome back!';
 console.log(message);
 var isBeginner = true;
+// isBeginner = 44; // gives an error since 44 is not of the same type (bool)
 var total = 0;
 var name = 'Sunny';
+var sentence = "My name is " + name + ".";
+console.log(sentence);
+// arrays
+var list1 = [1, 2, 3, 4, 5]; // first way to declare an array
+var list2 = [1, 2, 3, 4, 5]; // second way to declare an array
+// arrays with mixed values - use tuples
+var list3 = ['Sunny', 33];
+// any type
+var randomType = 10;
+randomType = 'Sunny';
--- main.ts
+++ main.ts
@@ -15,6 +15,23 @@ let message = 'Welcome back!';
 console.log(message);
 
 let isBeginner: boolean = true;
+// isBeginner = 44; // gives an error since 44 is not of the same type (bool)
 let total: number = 0;
 let name: string = 'Sunny';
+let sentence: string = `My name is ${name}.`;
+console.log(sentence);
+
+// arrays
+let list1: number[] = [1,2,3,4,5];          // first way to declare an array
+let list2: Array<number> = [1,2,3,4,5];    // second way to declare an array
+
+// arrays with mixed values - use tuples
+let list3: [string, number] = ['Sunny', 33];
+
+
+// any type
+let randomType: any = 10;
+randomType = 'Sunny';
+
+
 
