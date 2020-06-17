2892fe2298566251f826575deba257ec1eab0461
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-06-16 18:27:18 -0700

    TS Hello World!

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 // my first TypeScript program
-// compile program on terminal: tsc main.ts
-// run code using Node: node main.js
+// compile program on terminal:
+//   tsc main.ts
+// run code using Node:
+//   node main.js
 
 let message = 'Hello World!';
 console.log(message);
