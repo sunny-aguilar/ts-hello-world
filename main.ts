07c9948ffc6003cbbebd527c7e14cea3e310e56f
Sunny Aguilar <sunny.aguilar@gmail.com>
2020-06-16 18:29:39 -0700

    TS Hello World!

--- main.ts
+++ main.ts
@@ -4,6 +4,13 @@
 // run code using Node:
 //     node main.js
 
+// this treats this file as a module otherwise it is treated
+// as a script which shares the global scope with any other
+// scripts and therefore causing errors (duplicate variable
+// names (errase export{} to test)
+export {}
+
+
 let message = 'Hello World!';
 console.log(message);
 
